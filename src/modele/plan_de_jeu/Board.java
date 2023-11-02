92870a80bb64ce83597b7fe92653d321cc95b9e4
Renaud Morin <renaudmorin2001@gmail.com>
2023-11-02 19:31:20 -0400

    ploblem

--- src/modele/plan_de_jeu/Board.java
+++ src/modele/plan_de_jeu/Board.java
@@ -4,4 +4,23 @@ public class Board {
 
     private int[][] tiles;
 
+    private static Map<Integer, String> letters;
+    {
+        letters = new HashMap<>();
+        letter.put(1, "A");
+        letter.put(2, "B");
+        letter.put(3, "C");
+        letter.put(4, "D");
+        letter.put(5, "E");
+        letter.put(6, "F");
+        letter.put(7, "G");
+        letter.put(8, "H");
+        letter.put(9, "I");
+        letter.put(10, "J");
+        letter.put(11, "K");
+        letter.put(12, "L");
+        letter.put(13, "M");
+    }
+
+    public static String getLetterValue(Integer key) { return this.letters.get(key); }
 }
--- src/modele/plan_de_jeu/Position.java
+++ src/modele/plan_de_jeu/Position.java
@@ -1,5 +1,20 @@
 package modele.plan_de_jeu;
 
 public class Position {
+    private Integer i;
+    private Integer j;
+    private String boardPosition;
+
+
+
+    public Integer getI() { return this.i; }
+    public Integer getJ() { return this.j; }
+    public Integer getBoardPosition() { return this.boardPosition; }
+
+    public Position(Integer i, Integer j){
+        this.i = i;     
+        this.j = j;     
+        this.boardPosition = Board.getLetterValue(i) + this.j;
+    }
 
 }
