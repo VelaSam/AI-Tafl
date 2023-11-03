96d4c253b951cf20c1e3a0826927cb348cfe5f32
VelaSam <svelasco514@gmail.com>
2023-11-02 20:21:55 -0400

    change Integrals to integrals

--- src/modele/plan_de_jeu/Position.java
+++ src/modele/plan_de_jeu/Position.java
@@ -7,14 +7,14 @@ public class Position {
     private int j;
     private String boardPosition;
 
-    public Integer getI() { return this.i; }
-    public Integer getJ() { return this.j; }
-    public String getBoardPosition() { return this.boardPosition; }
 
-    public Position(Integer i, Integer j){
-        this.i = i;     
-        this.j = j;     
+    public Position(int i, int j){
+        this.i = i;
+        this.j = j;
         this.boardPosition = Helpers.getLetterValue(j) + this.i;
     }
+    public int getI() { return this.i; }
+    public int getJ() { return this.j; }
+    public String getBoardPosition() { return this.boardPosition; }
 
 }
