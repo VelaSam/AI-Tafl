8e3c20d77be8a756c9debf6a7d67edb162caa31b
Samuel Velasco <93353011+VelaSam@users.noreply.github.com>
2023-11-07 12:40:01 -0500

    added playMove method to modify board after move played
    THE BOARD WILL NOT MODIFY IF A PIECE DIES

--- src/modele/plan_de_jeu/Board.java
+++ src/modele/plan_de_jeu/Board.java
@@ -1,5 +1,7 @@
 package modele.plan_de_jeu;
 
+import modele.helpers.Helpers;
+
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -11,17 +13,18 @@ public class Board {
     private TileState maxPlayer;
     private TileState minPlayer;
 
+    public static int iterations = 0;
+
 
     public Board(String[] boardValues, TileState couleur) {
         initializeNewBoard(boardValues);
         this.maxPlayer = couleur;
         this.minPlayer = this.maxPlayer == TileState.NOIR ? TileState.ROUGE : TileState.NOIR;
-
     }
 
     public void initializeNewBoard(String[] boardValues) {
         tiles = new Tile[WIDTH][WIDTH];
-        int x = 0, y = 0, z = 0;
+        int z = 0;
         for (int j = 0; j < WIDTH; j++) {
             for(int i = 0 ; i < WIDTH; i++){
                 tiles[i][j] = new Tile(new Position(i, j), Integer.parseInt(boardValues[z]));
@@ -87,6 +90,62 @@ public class Board {
             positions.put(tile, availablePositions);
         }
     }
+
+
+    //Va recevoir un move tel que A5 - B5 et va ensuite modifier le board en consequence
+    public void playMoveOnBoard(String move) {
+        iterations++;
+        move = move.replace(" ", "");
+        String beginningSpace = move.split("-")[0];
+        String destinationSpace = move.split("-")[1];
+
+        Tile beginningTile = tiles[Helpers.getNumberValue(String.valueOf(beginningSpace.charAt(0)))][Integer.parseInt(beginningSpace.substring(1))-1];
+        Tile destinationTile = tiles[Helpers.getNumberValue(String.valueOf(destinationSpace.charAt(0)))][Integer.parseInt(beginningSpace.substring(1))-1];
+
+        TileState moverState = beginningTile.getState();
+
+        if (beginningTile.getState() == TileState.EMPTY) {
+            throw new IllegalArgumentException("Beginning tile: tiles[" + beginningTile.getX() + "][" + beginningTile.getY() + "] is empty. Cannot make a move.");
+        }
+
+        if (destinationTile.getState() != TileState.EMPTY) {
+            throw new IllegalArgumentException("Cannot move on a tile: tiles[" + beginningTile.getX() + "][" + beginningTile.getY() + "]  already containing a piece");
+        }
+
+        if(destinationTile.isMarkedX() && beginningTile.getState() != TileState.ROI_NOIR){
+            throw new IllegalArgumentException("A non king piece cannot move on a X tile: tiles[" + beginningTile.getX() + "][" + beginningTile.getY() + "]  ");
+        }
+
+        beginningTile.setState(TileState.EMPTY);
+        destinationTile.setState(moverState);
+
+        // removeable
+        System.out.println(this);
+    }
+
+
+    public String toString(){
+        StringBuilder boardStr = new StringBuilder("");
+
+        if(iterations == 0){
+            boardStr.append("Game start \n");
+        }else{
+            boardStr.append("Move number: ").append(iterations).append(" \n");
+        }
+
+        for(int i = WIDTH-1; i >= 0; i--){
+            for(int j = 0; j < WIDTH; j++){
+                String state;
+                boardStr.append(tiles[j][i].getState()).append(" ");
+            }
+        boardStr.append("\n");
+        }
+
+//        boardStr = new StringBuilder(boardStr).reverse().toString();
+
+        return boardStr.toString();
+    }
+
 
     private boolean isBlockedTile(Tile tile){
         //if the tile has a piece on it OR has an X marked on it
