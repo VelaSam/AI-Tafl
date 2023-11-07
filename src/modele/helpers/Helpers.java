bd56ecc09596905fff80a6613c8beeb94469484c
Samuel Velasco <93353011+VelaSam@users.noreply.github.com>
2023-11-07 12:38:52 -0500

    Modified helper

--- src/modele/helpers/Helpers.java
+++ src/modele/helpers/Helpers.java
@@ -4,26 +4,45 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Helpers {
-    public static Map<Integer, String> letters;
+    public static Map<Integer, String> letterFromNum;
+    public static Map<String, Integer> numFromLetter;
+
 
     static {
-        letters = new HashMap<>();
-        letters.put(0, "A");
-        letters.put(1, "B");
-        letters.put(2, "C");
-        letters.put(3, "D");
-        letters.put(4, "E");
-        letters.put(5, "F");
-        letters.put(6, "G");
-        letters.put(7, "H");
-        letters.put(8, "I");
-        letters.put(9, "J");
-        letters.put(10, "K");
-        letters.put(11, "L");
-        letters.put(12, "M");
+        letterFromNum = new HashMap<>();
+        letterFromNum.put(0, "A");
+        letterFromNum.put(1, "B");
+        letterFromNum.put(2, "C");
+        letterFromNum.put(3, "D");
+        letterFromNum.put(4, "E");
+        letterFromNum.put(5, "F");
+        letterFromNum.put(6, "G");
+        letterFromNum.put(7, "H");
+        letterFromNum.put(8, "I");
+        letterFromNum.put(9, "J");
+        letterFromNum.put(10, "K");
+        letterFromNum.put(11, "L");
+        letterFromNum.put(12, "M");
+
+        numFromLetter = new HashMap<>();
+        numFromLetter.put("A", 0);
+        numFromLetter.put("B", 1);
+        numFromLetter.put("C", 2);
+        numFromLetter.put("D", 3);
+        numFromLetter.put("E", 4);
+        numFromLetter.put("F", 5);
+        numFromLetter.put("G", 6);
+        numFromLetter.put("H", 7);
+        numFromLetter.put("I", 8);
+        numFromLetter.put("J", 9);
+        numFromLetter.put("K", 10);
+        numFromLetter.put("L", 11);
+        numFromLetter.put("M", 12);
     }
 
     public static String getLetterValue(Integer key) {
-        return letters.get(key);
+        return letterFromNum.get(key);
     }
+
+    public static int getNumberValue(String key){return numFromLetter.get(key);}
 }
